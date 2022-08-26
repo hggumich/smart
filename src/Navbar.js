3c3364ad792aa7674189d0881aabc89b6d8de0d1
Hector Garza <hggumich@gmail.com>
2022-08-26 16:51:47 -0400

    init commit

---
+++ src/Navbar.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
+
+export const Navbar = () => {
+  return (
+    <nav className="nav">
+      <Link to="/home" className="site-title">
+        Chemical Processing
+      </Link>
+      <ul>
+        <CustomLink to="/home">Home</CustomLink>
+        <CustomLink to="/about">About</CustomLink>
+        <CustomLink to="/contact">Contact</CustomLink>
+      </ul>
+    </nav>
+  );
+};
+
+function CustomLink({ to, children, ...props }) {
+  const resolvedPath = useResolvedPath(to);
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true });
+  return (
+    <li className={isActive ? "active" : ""}>
+      <Link to={to} {...props}>
+        {children}
+      </Link>
+    </li>
+  );
+}
