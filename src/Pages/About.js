d12b888770a1f793009469ff214dd3d13d415e38
Hector Garza <hggumich@gmail.com>
2022-09-02 16:29:29 -0400

    add about us info

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,5 +1,34 @@
+import { Box, Grid, Typography } from "@mui/material";
 import React from "react";
 
 export const About = () => {
-  return <div>About</div>;
+  return (
+    <Box display="flex">
+      <Grid container>
+        <Grid item xs={12}>
+          <Typography variant="h3" align="center">
+            About Us
+          </Typography>
+        </Grid>
+        <Grid item xs={12} p={2}></Grid>
+        <Grid item xs={12}>
+          <Typography variant="body1">
+            SmartXchain, LLC is an IT solutions services provider registered in
+            North Carolina. We develop web apps using blockchain technology for
+            businesses to handle aerospace industry audits efficiently; create
+            digital identify infrastructure for a small business engaged in R&D;
+            assist seeding for SBIR/STTR programs; and sell services using
+            datagraph to businesses. Our goal is to improve businesses’
+            efficiency by coordinating the various entities’ efforts in the
+            supply chain. This can result in a company achieving a competitive
+            advantage over its rivals and enhancing the quality of its products,
+            which can lead to increased sales and revenue. Our management is
+            highly experienced in handling NADCAP, AN9100 and customer audits,
+            setting up and starting chemica processes for Aerospace, Distributed
+            Ledger Technologies and Digital Identify Infrastructure.
+          </Typography>
+        </Grid>
+      </Grid>
+    </Box>
+  );
 };
--- src/Pages/Layout.js
+++ src/Pages/Layout.js
@@ -4,16 +4,28 @@ import { Contact } from "./Contact";
 import { Home } from "./Home";
 import { Routes, Route } from "react-router-dom";
 import "../styles.css";
+import { Divider, Paper, Stack, Box, Grid } from "@mui/material";
 
 export const Layout = () => {
   return (
-    <div className="container">
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </div>
+    <Box display="flex" p={2}>
+      <Grid container my={4}>
+        <Grid item xs={12}></Grid>
+        <Grid item xs={6} spacing={1}>
+          <Paper sx={{ padding: "1rem" }} elevation={4}></Paper>
+        </Grid>
+        <Grid item xs={6} spacing={1}>
+          <Paper sx={{ padding: "1rem" }} elevation={4}>
+            <Routes>
+              <Route index element={<Home />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/contact" element={<Contact />} />
+            </Routes>
+          </Paper>
+        </Grid>
+        <Grid item xs={12}></Grid>
+      </Grid>
+    </Box>
   );
 };
